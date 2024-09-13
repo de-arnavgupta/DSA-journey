02c78e7766a5d260ff25f27d021c57cd473be055
noir <noir@odin.com>
2024-09-13 17:07:27 +0530

    Sum of three with unique constraints

---
+++ Loops/EasyQuestions/SumofThree.java
@@ -0,0 +1,26 @@
+package codeforces.loops.easyQuestions;
+
+import java.util.Scanner;
+
+public class SumofThree {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int K = scanner.nextInt();
+        int S = scanner.nextInt();
+
+        int count = 0;
+
+        for (int X = 0; X <= K; X++) {
+            for (int Y = 0; Y <= K; Y++) {
+                int Z = S - X - Y;
+
+                if (Z >= 0 && Z <= K) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
+
