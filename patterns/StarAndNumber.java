45f8a4a7e1960b9948392f4b7747afdc62bc27de
noir <noir@odin.com>
2024-09-20 14:57:03 +0530

    pattern question of Star and Number

---
+++ patterns/StarAndNumber.java
@@ -0,0 +1,23 @@
+package DSA.patterns;
+
+import java.util.Scanner;
+
+public class StarAndNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+        for(int i = 1; i <= row; i++) {
+            int num = 1;
+            for(int j = 1; j <= i; j++) {
+                if(j % 2 != 0) {
+                    System.out.print(num);
+                    num++;
+                }
+                else{
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
