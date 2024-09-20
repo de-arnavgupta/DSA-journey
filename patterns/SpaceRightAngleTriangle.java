1c4165d081e97e65a116ad00e9fed81e3d5e8229
noir <noir@odin.com>
2024-09-20 15:42:26 +0530

    variant of space and right angle triangle pattern

--- patterns/SpaceRightAngleTriangle.java
+++ patterns/SpaceRightAngleTriangle.java
@@ -7,10 +7,22 @@ public class SpaceRightAngleTriangle {
         Scanner sc = new Scanner(System.in);
         int row = sc.nextInt();
         for(int i = 1; i <= row; i++) {
-            for(int j = 1; j <= row-i; j++) {
+            for(int j = 1; j < i; j++) {
                 System.out.print(" ");
             }
-            for(int j = 1; j <= i; j++) {
+            for(int j = 1; j <= row-i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        //Space Right Angle Triangle Modified
+        for(int i = 1; i <= row; i++) {
+            for(int j = 1; j < i; j++) {
+                System.out.print(" ");
+            }
+            for(int j = row; j >= i; j--) {
                 System.out.print("*");
             }
             System.out.println();
