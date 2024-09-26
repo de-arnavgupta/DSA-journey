1aba04857c2ad1a1d63631e46d124664f739a150
noir <noir@odin.com>
2024-09-26 13:23:40 +0530

    pyramid pattern

---
+++ patterns/Pyramid.java
@@ -0,0 +1,26 @@
+package DSA.patterns;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Pyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int test = sc.nextInt();
+        for(int i = 0; i < test; i++) {
+            int num = sc.nextInt();
+            for(int j = 1; j <= num; j++) {
+                for(int k = num; k > j; k--) {
+                    System.out.print(" ");
+                }
+                for(int k = 1; k <= j; k++) {
+                    System.out.print("*");
+                    if(k != j) {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
+}
