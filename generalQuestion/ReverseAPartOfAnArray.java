5a75af22ce12a9cd4ef7d436a27e31ff9f119a71
noir <noir@odin.com>
2024-09-27 15:11:36 +0530

    made minor changes

--- generalQuestion/ReverseAPartOfAnArray.java
+++ generalQuestion/ReverseAPartOfAnArray.java
@@ -3,25 +3,36 @@ package DSA.generalQuestion;
 import java.util.Scanner;
 
 public class ReverseAPartOfAnArray {
+    public static void inputArray(Scanner sc, int size){
+        int[] arr = new int[size];
+        for(int i = 0; i < size; i++){
+            arr[i] = sc.nextInt();
+        }
+    }
     public static void reverse(int[] arr, int start, int end){
         for(int i = start; i < end; i++, end--){
             int temp = arr[i];
             arr[i] = arr[end];
             arr[end] = temp;
         }
+    }
+    public static void printReversedArr(int[] arr){
         for (int j : arr) {
             System.out.print(j + " ");
         }
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int size = sc.nextInt();
         int[] arr = new int[size];
-        for(int i = 0; i < size; i++){
-            arr[i] = sc.nextInt();
-        }
+
+        inputArray(sc, size);
+
         int start = sc.nextInt();
         int end = sc.nextInt();
+
         reverse(arr, start, end);
+        printReversedArr(arr);
     }
 }
