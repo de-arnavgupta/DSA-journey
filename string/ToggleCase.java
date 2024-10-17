966e5dd9d874fc4e3304876bef7bb9b0e41be9d1
noir <noir@odin.com>
2024-10-17 13:58:37 +0530

    used StringBuilder

--- string/ToggleCase.java
+++ string/ToggleCase.java
@@ -4,14 +4,17 @@ import java.util.Scanner;
 
 public class ToggleCase {
     public static String toggleCase(String str) {
-        for (int i = 0; i < str.length(); i++) {
-            if(str.charAt(i) >= 'A' && str.charAt(i) <= 'Z') {
-                str = str.substring(0, i) + (char)(str.charAt(i) + 32) + str.substring(i + 1);
+        StringBuilder sb = new StringBuilder(str);
+        for(int i = 0; i < sb.length(); i++) {
+            char c = sb.charAt(i);
+            if(c >= 'A' && c <= 'Z') {
+                sb.setCharAt(i, (char)(c + 32));
             }
-            else if(str.charAt(i) >= 'a' && str.charAt(i) <= 'z') {
-                str = str.substring(0, i) + (char)(str.charAt(i) - 32) + str.substring(i + 1);
+            else if(c >= 'a' && c <= 'z') {
+                sb.setCharAt(i, (char)(c - 32));
             }
         }
+        str = sb.toString();
         return str;
     }
     public static void main(String[] args) {
