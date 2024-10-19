76d5b251fedb828742ef6d5929ae01dba098bd9e
noir <noir@odin.com>
2024-10-19 10:41:13 +0530

    Container With Most Water

---
+++ leetcode/ContainerWithMostWater.java
@@ -0,0 +1,22 @@
+package DSA.leetcode;
+
+public class ContainerWithMostWater {
+    public static int maxArea(int[] height) {
+        int max = 0;
+        int i = 0;
+        int j = height.length - 1;
+        while(i < j) {
+            max = Math.max(max, Math.min(height[i], height[j]) * (j - i));
+            if(height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        int[] height = {1,8,6,2,5,4,8,3,7};
+        System.out.println(maxArea(height));
+    }
+}
